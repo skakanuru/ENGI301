0c8c61fd9b0f9a894cb8c4918627138f33ec9e71
skakanuru <68570954+skakanuru@users.noreply.github.com>
2023-11-18 15:56:33 -0600

    Create run.sh

---
+++ Project_1/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# ----------------------------------------------------------------
+# <Same documentation / copyright as above>
+# ----------------------------------------------------------------
+cd /var/lib/cloud9/ENGI301/python/blink_led
+
+python3 Final_301_Project.py
